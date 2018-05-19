4e737f2b83ec1c6874acab31b1df1765d4fa4bc2
Julie <Juliapretzlaff@gmail.com>
2018-05-19 14:56:37 -0700

    PHP arrays

--- app/public/wp-content/themes/pretendUniversityTheme/index.php
+++ app/public/wp-content/themes/pretendUniversityTheme/index.php
@@ -5,4 +5,11 @@ echo "<p>Hi, my name is $name and my favorite color is $color</p>";
 greeting('Julie', 'blue');
 greeting('Austin', 'green');
 ?>
+<h1><?php bloginfo('name'); ?></h1>
+<p><?php bloginfo('description'); ?></p>
 
+
+<?php
+$names = array('Julie', 'Allen', 'Austin');
+?>
+<p>Hi, my name is <?php echo $names[0] ?></p>
