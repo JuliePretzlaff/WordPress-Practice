913eb9eaf43b79bf17e761e27233b39fe6f25fc7
Julie <Juliapretzlaff@gmail.com>
2018-06-04 12:40:34 -0700

    starting queries

--- app/public/wp-content/themes/pretendUniversityTheme/front-page.php
+++ app/public/wp-content/themes/pretendUniversityTheme/front-page.php
@@ -44,6 +44,13 @@
   <div class="full-width-split__two">
     <div class="full-width-split__inner">
       <h2 class="headline headline--small-plus t-center">From Our Blogs</h2>
+      <?php 
+      while(have_posts()){
+        the_post();
+        ?>
+        <li><?php the_title(); ?></li>
+      <?php }
+      ?>
 
       <div class="event-summary">
         <a class="event-summary__date event-summary__date--beige t-center" href="#">
