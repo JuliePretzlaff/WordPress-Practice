eb4ed61feddd7e373c8789b88b2a55d9dec54431
Julie <Juliapretzlaff@gmail.com>
2018-06-04 11:55:28 -0700

    adding author, date, and category to blog posts

--- app/public/wp-content/themes/pretendUniversityTheme/index.php
+++ app/public/wp-content/themes/pretendUniversityTheme/index.php
@@ -19,7 +19,7 @@ the_post();
 <h2 class="headline headline--medium headline--post-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?> </a></h2>
 
 <div class="metabox">
-<p>Posted by Julie on today in news</p>
+<p>Posted by <?php the_author_posts_link();?> on <?php the_time('n.j.y');?> in <?php echo get_the_category_list(', ');?></p>
 </div>
 
 <div class= "generic-content">
